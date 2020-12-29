d58588aa6f6f573a2fc3f146e2752ee6352bae17
sathish-csk <cskpacet@gmail.com>
2020-12-29 13:42:11 +0530

    upload the letter.py file to repository

---
+++ letters.py
@@ -0,0 +1,272 @@
+import math
+from swampy.TurtleWorld import *
+
+def square(t, length):
+    for i in range(4):
+        fd(t, length)
+        lt(t)
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
+
+def polygon(t, n, length):
+    angle = 360.0/n
+    polyline(t, n, length, angle)
+
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    lt(t, step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    rt(t, step_angle/2)
+
+
+def circle(t, r):
+    arc(t, r, 360)
+
+
+def fdlt(t, n, angle=90):
+    """forward and left"""
+    fd(t, n)
+    lt(t, angle)
+
+def fdbk(t, n):
+    """forward and back, ending at the original position"""
+    fd(t, n)
+    bk(t, n)
+
+def skip(t, n):
+    """lift the pen and move"""
+    pu(t)
+    fd(t, n)
+    pd(t)
+
+def stump(t, n, angle=90):
+    """make a vertical line and leave the turtle at the top, facing right"""
+    lt(t)
+    fd(t, n)
+    rt(t, angle)
+
+def hollow(t, n):
+    """move the turtle vertically and leave it at the top, facing right"""
+    lt(t)
+    skip(t, n)
+    rt(t)
+
+
+# LEVEL 2 PRIMITIVES use primitives from Levels 0 and 1
+# to draw posts (vertical elements) and beams (horizontal elements)
+# Level 2 primitives ALWAYS return the turtle to the original
+# location and direction.
+
+def post(t, n):
+    """make a vertical line and return to the original position"""
+    lt(t)
+    fdbk(t, n)
+    rt(t)
+
+def beam(t, n, height):
+    """make a horizontal line at the given height and return."""
+    hollow(t, n*height)
+    fdbk(t, n)
+    hollow(t, -n*height)
+
+
+def hangman(t, n, height):
+    stump(t, n * height)
+    fdbk(t, n)
+    lt(t)
+    bk(t, n*height)
+    rt(t)
+
+def diagonal(t, x, y):
+    from math import atan2, sqrt, pi
+    angle = atan2(y, x) * 180 / pi
+    dist = sqrt(x**2 + y**2)
+    lt(t, angle)
+    fdbk(t, dist)
+    rt(t, angle)
+
+def vshape(t, n, height):
+    diagonal(t, -n/2, height*n)
+    diagonal(t, n/2, height*n)
+
+def bump(t, n, height):
+    stump(t, n*height)
+    arc(t, n/2.0, 180)
+    lt(t)
+    fdlt(t, n*height+n)
+
+def draw_a(t, n):
+    diagonal(t, n/2, 2*n)
+    beam(t, n, 1)
+    skip(t, n)
+    diagonal(t, -n/2, 2*n)
+
+def draw_b(t, n):
+    bump(t, n, 1)
+    bump(t, n, 0)
+    skip(t, n/2)
+
+def draw_c(t, n):
+    hangman(t, n, 2)
+    fd(t, n)
+
+def draw_d(t, n):
+    bump(t, 2*n, 0)
+    skip(t, n)
+
+def draw_ef(t, n):
+    hangman(t, n, 2)
+    hangman(t, n, 1)
+
+def draw_e(t, n):
+    draw_ef(t, n)
+    fd(t, n)
+
+def draw_f(t, n):
+    draw_ef(t, n)
+    skip(t, n)
+
+def draw_g(t, n):
+    hangman(t, n, 2)
+    fd(t, n/2)
+    beam(t, n/2, 2)
+    fd(t, n/2)
+    post(t, n)
+
+def draw_h(t, n):
+    post(t, 2*n)
+    hangman(t, n, 1)
+    skip(t, n)
+    post(t, 2*n)
+
+def draw_i(t, n):
+    beam(t, n, 2)
+    fd(t, n/2)
+    post(t, 2*n)
+    fd(t, n/2)
+
+def draw_j(t, n):
+    beam(t, n, 2)
+    arc(t, n/2, 90)
+    fd(t, 3*n/2)
+    skip(t, -2*n)
+    rt(t)
+    skip(t, n/2)
+
+def draw_k(t, n):
+    post(t, 2*n)
+    stump(t, n, 180)
+    vshape(t, 2*n, 0.5)
+    fdlt(t, n)
+    skip(t, n)
+
+def draw_l(t, n):
+    post(t, 2*n)
+    fd(t, n)
+
+def draw_n(t, n):
+    post(t, 2*n)
+    skip(t, n)
+    diagonal(t, -n, 2*n)
+    post(t, 2*n)
+
+def draw_m(t, n):
+    post(t, 2*n)
+    draw_v(t, n)
+    post(t, 2*n)
+
+def draw_o(t, n):
+    skip(t, n)
+    circle(t, n)
+    skip(t, n)
+
+def draw_p(t, n):
+    bump(t, n, 1)
+    skip(t, n/2)
+
+def draw_q(t, n):
+    draw_o(t, n)
+    diagonal(t, -n/2, n)
+
+def draw_r(t, n):
+    draw_p(t, n)
+    diagonal(t, -n/2, n)
+
+def draw_s(t, n):
+    fd(t, n/2)
+    arc(t, n/2, 180)
+    arc(t, n/2, -180)
+    fdlt(t, n/2, -90)
+    skip(t, 2*n)
+    lt(t)
+
+def draw_t(t, n):
+    beam(t, n, 2)
+    skip(t, n/2)
+    post(t, 2*n)
+    skip(t, n/2)
+
+def draw_u(t, n):
+    post(t, 2*n)
+    fd(t, n)
+    post(t, 2*n)
+
+def draw_v(t, n):
+    skip(t, n/2)
+    vshape(t, n, 2)
+    skip(t, n/2)
+
+def draw_w(t, n):
+    draw_v(t, n)
+    draw_v(t, n)
+
+def draw_x(t, n):
+    diagonal(t, n, 2*n)
+    skip(t, n)
+    diagonal(t, -n, 2*n)
+
+def draw_v(t, n):
+    skip(t, n/2)
+    diagonal(t, -n/2, 2*n)
+    diagonal(t, n/2, 2*n)
+    skip(t, n/2)
+
+def draw_y(t, n):
+    skip(t, n/2)
+    stump(t, n)
+    vshape(t, n, 1)
+    rt(t)
+    fdlt(t, n)
+    skip(t, n/2)
+
+def draw_z(t, n):
+    beam(t, n, 2)
+    diagonal(t, n, 2*n)
+    fd(t, n)
+
+def draw_(t, n):
+    # draw a space
+    skip(t, n)
+
+if __name__ == '__main__':
+    world = TurtleWorld()
+
+    # create and position the turtle
+    size = 20
+    bob = Turtle()
+    bob.delay = 0.01
+    for f in [draw_a, draw_b, draw_c,draw_d, draw_e,draw_f, draw_g,draw_h, draw_i,draw_j, draw_k,draw_l, draw_m,draw_n, draw_o,draw_p, draw_q,draw_r, draw_s,draw_t, draw_u,draw_v, draw_w,draw_x, draw_y,draw_z]:
+        f(bob, size)
+        skip(bob, size)
+
+    wait_for_user()
